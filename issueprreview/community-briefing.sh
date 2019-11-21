60f952fb9c77451fff3ea4834374aa42acc675f1
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2019-11-21 14:58:03 +0100

    Update Scripts for running on Jenkins

--- issueprreview/community-briefing.sh
+++ issueprreview/community-briefing.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-
-export myfolder="/home/iranzo/DEVEL/RH/syseng"
+export myfolder="$HOME/DEVEL/RH/syseng"
+mkdir -p $myfolder
 
 DATE="$1"
 
 if [ "${DATE}" == "" ]; then
     echo "Provide date as 2019-06-01 as first argument"
-    exit 1
+    DATE="$(date -d 'last Thursday - 1 day' +%F)"
 fi
 
-(for repo in kube/demo kube/community kube/kubevirt-tutorial kube/cloud-image-builder kube/katacoda-kubevirt metal3/metal3-io.github.io kube/kubevirt.github.io; do
+echo -e "Weekly report up-to: $DATE\n" |tee -a weekly-report.log
+
+# Prepare GHI
+rm -Rf ghi
+git clone https://github.com/stephencelis/ghi.git
+PATH="`pwd`/ghi:$PATH"
+
+(for repo in kubevirt/demo kubevirt/community kubevirt/kubevirt-tutorial kubevirt/cloud-image-builder kubevirt/katacoda-scenarios metal3-io/metal3-io.github.io kubevirt/kubevirt.github.io; do
     cd $myfolder
+    rm -Rf $repo > /dev/null 2>&1
+    git clone "https://github.com/$repo" "$repo" > /dev/null 2>&1
     cd $repo
     echo -e "\n$repo"
     echo " - PR closed: $(ghi list --since ${DATE} -s closed -p|grep -v "^#" |grep -v None|wc -l)"
@@ -22,4 +31,4 @@ fi
     echo " - Still open Issues"
     ghi list -s open -P 2>&1|grep -v "^#" |grep -v Ignoring|awk -v REPO="$repo" '/[0-9]+/ {print "    ** ["$1"|https://github.com/" REPO "/issues/" $1 "]"$0}'
 done
-) 2>&1|grep -v Ignoring
+) 2>&1|grep -v Ignoring |grep -v "Insecure world writable dir"|tee -a weekly-report.log
